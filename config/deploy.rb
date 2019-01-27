606e5aa3516bedf848ecc85f4e13e8765a25ba39
John Brayton <jbrayton@goldenhillsoftware.com>
2019-01-27 17:32:07 -0500

    fixed directory location.

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@bitbucket.org:johnbrayton/monitor-webapp.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/sites/monitor-webapp"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
