db57f3f35c122f9ce4c143604052fd4aba23fa60
John Brayton <jbrayton@goldenhillsoftware.com>
2019-01-27 18:56:42 -0500

    update deploy script

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,9 @@
 server '69.164.211.93', user: 'passenger', roles: %w{app web}, port: 14
 
+
+
 set :ssh_options, {
   forward_agent: true,
   keys: %w(/Users/jbrayton/.ssh/identities/server2017/id_rsa)
 }
+
