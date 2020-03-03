b871878b1803df4d7e3fbe8191a831428e3f3f2a
John Brayton <jbrayton@goldenhillsoftware.com>
2020-03-03 12:53:10 -0500

    removed the Capfile lock

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,3 @@
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.11.0"
-
 set :application, "monitor-webapp"
 set :repo_url, "git@bitbucket.org:johnbrayton/monitor-webapp.git"
 
