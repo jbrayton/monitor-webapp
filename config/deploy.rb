4196c7bfab4bd78edacd746f47e4caf796107430
John Brayton <jbrayton@goldenhillsoftware.com>
2021-10-09 10:37:41 -0400

    changed git repo

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 set :application, "monitor-webapp"
-set :repo_url, "git@bitbucket.org:johnbrayton/monitor-webapp.git"
-set :branch, "master"
+set :repo_url, "git@github.com:jbrayton/monitor-webapp.git"
+set :branch, "main"
 set :deploy_to, "/monitor-webapp"
 set :ssh_options, { forward_agent: true, user: "passenger", auth_methods: ['publickey'], keys: %w(~/.ssh/privatekey.pem) }
