4ebc68310dc465b1489cfa70e7c555b1abb87f45
John Brayton <jbrayton@goldenhillsoftware.com>
2022-09-09 17:56:33 -0400

    change deploy directory

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 set :application, "monitor-webapp"
 set :repo_url, "git@github.com:jbrayton/monitor-webapp.git"
 set :branch, "main"
-set :deploy_to, "/monitor-webapp"
+set :deploy_to, "/monitor"
 set :ssh_options, { forward_agent: true, user: "passenger", auth_methods: ['publickey'], keys: %w(~/.ssh/privatekey.pem) }
